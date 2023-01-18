3ab51f4043e7a524af66a7779c6d6c069c7d8214
diwa-kar <diwakarnehrudasan@gmail.com>
2023-01-18 12:13:45 +0530

    first commit

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => {
             <ul>
               <li>70 Washington Square South, New York, NY 10012, United States </li>
               <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
+              <li>Phone: +91 6383308393</li>
             </ul>
           </div>
         </div>
